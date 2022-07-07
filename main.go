68d89ebfe9a899e64a264acfdcd6a91cf36c5749
Brandon <82053246+bwoff11@users.noreply.github.com>
2022-07-07 10:22:24 -0500

    Middleware

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	v1 "github.com/bwoff11/frens/internal/api/v1"
 	"github.com/bwoff11/frens/internal/db"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
 	db.Connect()
 	api := fiber.New()
+	api.Use(logger.New())
+	api.Use(recover.New())
+
 	v1.AddRoutes(api)
 
 	api.Listen(":8080")
